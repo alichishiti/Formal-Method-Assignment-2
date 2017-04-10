67c25567194a666a38a41c88df87d7c23db31a16
gul zain <gulzainali@gmail.com>
2017-04-10 13:20:15 +0500

    chishti just lauvvvesss clock tower

--- SAT_solver/src/sat_solver/ClockTower.java
+++ SAT_solver/src/sat_solver/ClockTower.java
@@ -5,10 +5,86 @@
  */
 package sat_solver;
 
+
+
+import static java.lang.Math.pow;
+import java.util.*;  
 /**
  *
  * @author gul
  */
 public class ClockTower {
+    
+    char[] binaryOperators;
+    String expression;
+   ArrayList<Character> propositionList;
+   HashMap<Character,Integer> propositionToValue;
+    
+    public ClockTower(String expression){
+        this.propositionToValue= new HashMap<Character,Integer>();
+        this.propositionList = new ArrayList<Character>();
+        this.binaryOperators=new char[]{'&', '|', '>', '='};
+        this.expression=expression;
+    }
+    
+    public void extractPropositions(){
+//    Character.isLetter(element)
+        char element;
+        boolean addProposition = true;
+        for(int i = 0 ; i < this.expression.length();i++){
+            element = this.expression.charAt(i);
+            
+            if(Character.isLetter(element)){
+                addProposition = true;
+                
+                for (int j = 0; j < this.propositionList.size(); j++){
+                    if(this.propositionList.get(j) == element){
+                        addProposition = false;
+                        break;
+                    }        
+                }
+
+                if(addProposition){
+                    this.propositionList.add(element);
+                    addProposition = true;
+                }
+            }
+        }
+        
+        System.out.println(propositionList);
+    }
+    
+    public void MapPropositionValues(){
+        
+        
+        for(int i = 0 ; i< pow(2,propositionList.size());i++){
+            String binary= Integer.toBinaryString(i);
+            binary= append(binary,propositionList.size());
+            for(int j =0 ; j< binary.length(); j++){
+                propositionToValue.put(propositionList.get(j), 
+                        Integer.parseInt(binary.substring(j,j+1)));
+                }
+            System.out.println(propositionToValue);
+        }
+        
+        
+    }
+    
+    public void Calculate(){
+       
+    
+    }
+    
+    private String append(String binary, int length){
+    
+        while(binary.length()!=length){
+        
+            binary="0"+binary;
+        }
+        return binary;
+    }
+    
+    
+    
     
 }
--- SAT_solver/src/sat_solver/SAT_solver.java
+++ SAT_solver/src/sat_solver/SAT_solver.java
@@ -2,9 +2,13 @@ package sat_solver;
 public class SAT_solver {
     public static void main(String[] args) {
         
-        String b = "p|(q&r)";
+        String b = "~p|q";
         MakeCNF a = new MakeCNF(b);
-        System.out.print(a.makeCNF());
+        //System.out.print(a.makeCNF());
+        ClockTower chishti= new ClockTower(b);
+        chishti.extractPropositions();
+        chishti.MapPropositionValues();
+      
         
 //        a.Bijection();
 //        a.Implication();
